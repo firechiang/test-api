3eb7adfeb4286eaec8bca408d069fd02344fba82
firechiang <fire-chiang@outlook.com>
2026-03-23 23:41:04 +0800

    fix: url

--- paipai-admin-api/src/main/java/com/paipai/admin/api/config/WebMvcConfig.java
+++ paipai-admin-api/src/main/java/com/paipai/admin/api/config/WebMvcConfig.java
@@ -39,7 +39,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         config.addAllowedOriginPattern("*");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
         config.setMaxAge(3600L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
